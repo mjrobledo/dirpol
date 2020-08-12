cc97fb17fb4381170b360434a681276522d9c0e1
Robledo <mrobledo@financialsolutions.mx>
2020-08-11 21:05:41 -0500

    cambios Region

--- dirpol/Aplicacion/NewDesing/Regions/RegionCardVC.swift
+++ dirpol/Aplicacion/NewDesing/Regions/RegionCardVC.swift
@@ -8,7 +8,47 @@
 
 import UIKit
 
+protocol RegionCardVCDelegate {
+    func filter()
+    func goToProvince()
+    func goToCity()
+    func goToDistrito()
+}
+
 class RegionCardVC: UIViewController {
 
+    var delegate: RegionCardVCDelegate!
+    
     @IBOutlet weak var handleArea: UIView!
+    @IBOutlet weak var txtDepto: UITextField!
+    @IBOutlet weak var txtProvincia: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtDistrito: UITextField!
+ 
+     /*
+    override func viewDidLoad() {
+        self.viewDidLoad()
+        
+    }*/
+    
+    @IBAction func filter(_ sender: Any) {
+        if delegate != nil {
+            self.delegate.filter()
+        }
+    }
+    
+    @IBAction func goToProvince(_ sender: Any) {
+        self.delegate.goToProvince()
+    }
+    
+    @IBAction func goToCity(_ sender: Any) {
+        self.delegate.goToCity()
+    }
+    
+    @IBAction func goToDistrito(_ sender: Any) {
+        self.delegate.goToDistrito()
+    }
+    
+    
+    
 }
--- dirpol/Aplicacion/NewDesing/Regions/RegionsVC.swift
+++ dirpol/Aplicacion/NewDesing/Regions/RegionsVC.swift
@@ -21,6 +21,8 @@ class RegionsVC: UIViewController {
     var regionCard: RegionCardVC!
     var visualEffectView: UIVisualEffectView!
     
+    var marker:[GMSMarker] = []
+    
     var cardHeight:CGFloat = 400
     let cardHandleAreaHeight:CGFloat = 65
     
@@ -40,7 +42,30 @@ class RegionsVC: UIViewController {
                   btnMenu.action = #selector(SWRevealViewController.revealToggle(_:))
                   view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
               }
-
+        
+        viewMap.camera = GMSCameraPosition(latitude: -10.190200, longitude: -75.538139, zoom: 6)
+        viewMap.delegate = self
+        /*
+         -10.190200, -75.538139
+         */
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -16.355114, longitude: -71.540182)
+        marker.title = "Arequipa"
+        marker.snippet = "Peru"
+        marker.map = viewMap
+        marker.iconView = UIImageView(image: UIImage(named: "ic_marker_region"))
+        //marker.setValue("1", forKey: "id")
+        
+        let marker2 = GMSMarker()
+        marker2.position = CLLocationCoordinate2D(latitude: -9.800696, longitude: -76.175346)
+        marker2.iconView = UIImageView(image: UIImage(named: "ic_marker_region"))
+        marker2.title = "Huánuco"
+        marker2.snippet = "Peru"
+        //marker.setValue("2", forKey: "id")
+        marker2.map = viewMap
+        
+                 
+        
         // Do any additional setup after loading the view.
     }
     
@@ -62,123 +87,15 @@ class RegionsVC: UIViewController {
     */
 
 }
-// Mark: - Methods Card Menu
-extension RegionsVC {
-    func setupCard() {
-        visualEffectView = UIVisualEffectView()
-        visualEffectView.frame = self.view.frame
-        //self.view.addSubview(visualEffectView)
-        
-        regionCard = RegionCardVC(nibName:"RegionCardVC", bundle:nil)
-        self.addChild(regionCard)
-        self.view.addSubview(regionCard.view)
-        
-        cardHeight = 560
-        regionCard.view.frame = CGRect(x: 20, y: self.view.frame.height - cardHandleAreaHeight, width: self.view.bounds.width - 40, height: cardHeight)
-        
-        regionCard.view.clipsToBounds = true
-        
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleCardTap(recognzier:)))
-        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(self.handleCardPan(recognizer:)))
-        
-        regionCard.handleArea.addGestureRecognizer(tapGestureRecognizer)
-        regionCard.handleArea.addGestureRecognizer(panGestureRecognizer)
+
+extension RegionsVC: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        regionCard.txtDepto.text = marker.title        
+        return false // return false to display info window
     }
-    
-    @objc
-       func handleCardTap(recognzier:UITapGestureRecognizer) {
-           switch recognzier.state {
-           case .ended:
-               animateTransitionIfNeeded(state: nextState, duration: 0.9)
-           default:
-               break
-           }
-       }
-       
-       @objc
-       func handleCardPan (recognizer:UIPanGestureRecognizer) {
-           switch recognizer.state {
-           case .began:
-               startInteractiveTransition(state: nextState, duration: 0.9)
-           case .changed:
-               let translation = recognizer.translation(in: self.regionCard.handleArea)
-               var fractionComplete = translation.y / cardHeight
-               fractionComplete = cardVisible ? fractionComplete : -fractionComplete
-               updateInteractiveTransition(fractionCompleted: fractionComplete)
-           case .ended:
-               continueInteractiveTransition()
-           default:
-               break
-           }
-           
-       }
-       
-       func animateTransitionIfNeeded (state:CardState, duration:TimeInterval) {
-           if runningAnimations.isEmpty {
-               let frameAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
-                   switch state {
-                   case .expanded:
-                       self.regionCard.view.frame.origin.y = (self.view.frame.height + 60) - self.cardHeight
-                   case .collapsed:
-                       self.regionCard.view.frame.origin.y = self.view.frame.height - self.cardHandleAreaHeight
-                   }
-               }
-               
-               frameAnimator.addCompletion { _ in
-                   self.cardVisible = !self.cardVisible
-                   self.runningAnimations.removeAll()
-               }
-               
-               frameAnimator.startAnimation()
-               runningAnimations.append(frameAnimator)
-               
-               
-               let cornerRadiusAnimator = UIViewPropertyAnimator(duration: duration, curve: .linear) {
-                   switch state {
-                   case .expanded:
-                       self.regionCard.view.layer.cornerRadius = 12
-                   case .collapsed:
-                       self.regionCard.view.layer.cornerRadius = 12
-                   }
-               }
-               
-               cornerRadiusAnimator.startAnimation()
-               runningAnimations.append(cornerRadiusAnimator)
-               
-               let blurAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
-                   switch state {
-                   case .expanded:
-                       self.visualEffectView.effect = UIBlurEffect(style: .dark)
-                   case .collapsed:
-                       self.visualEffectView.effect = nil
-                   }
-               }
-               
-               blurAnimator.startAnimation()
-               runningAnimations.append(blurAnimator)
-               
-           }
-       }
-       
-       func startInteractiveTransition(state:CardState, duration:TimeInterval) {
-           if runningAnimations.isEmpty {
-               animateTransitionIfNeeded(state: state, duration: duration)
-           }
-           for animator in runningAnimations {
-               animator.pauseAnimation()
-               animationProgressWhenInterrupted = animator.fractionComplete
-           }
-       }
-       
-       func updateInteractiveTransition(fractionCompleted:CGFloat) {
-           for animator in runningAnimations {
-               animator.fractionComplete = fractionCompleted + animationProgressWhenInterrupted
-           }
-       }
-       
-       func continueInteractiveTransition (){
-           for animator in runningAnimations {
-               animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
-           }
-       }
+/*
+    func mapView(_ mapView: GMSMapView, didCloseInfoWindowOf marker: GMSMarker) {
+        marker.icon = UIImage(named: "map_marker_unselected")
+    }*/
 }
+
